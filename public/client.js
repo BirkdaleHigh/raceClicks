2dd9edcef090115f0d16fb4fa8cd0ba85a1c5a53
Mr J Bennett <jbennett@birkdalehigh.co.uk>
2016-03-16 13:12:59 +0000

    2 progress bars have buttons to be clicked to fill up.

--- public/client.js
+++ public/client.js
@@ -5,11 +5,25 @@ var Button = function(id){
 
   self.id = id || 1
   self.count = 0
-  document.getElementById(id).onclick = function(){ self.press() }
+  self.el = document.getElementById(id)
+  self.el.onclick = function(){ self.press() }
+  self.progress = document.querySelector("progress." + this.id)
 }
-Button.prototype.press = function(){
-  this.count += 1
-  socket.emit("button pressed", {"button": this.id, "count": this.count})
+Button.prototype = {
+  press: function(){
+    this.count += 1
+
+    if(this.progress.value >= this.progress.max){
+      this.reset()
+    }
+    this.progress.value = this.count
+
+    socket.emit("button pressed", {"button": this.id, "count": this.count})
+  },
+  reset: function(){
+    this.count = 0
+    this.progress.value = 0
+  }
 }
 
 var red = new Button("red")
