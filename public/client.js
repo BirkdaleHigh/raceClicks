54c78b7c4e918b147c0fee3186303e8fc0fe27ae
Mr J Bennett <jbennett@birkdalehigh.co.uk>
2016-03-16 11:43:00 +0000

    turn the button into a reusable button object

--- public/client.js
+++ public/client.js
@@ -1,7 +1,19 @@
 var socket = io()
 
-document.querySelector('#button').addEventListener('click', function(){
-  console.log('boop.')
-  socket.emit("button pressed", {"button": 1})
+var Button = function(id){
+  this.id = id || 1
+  this.count = 0
+}
+Button.prototype.press = function(){
+  this.count += 1
+  socket.emit("button pressed", {"button": this.id})
+}
+
+var red = new Button("red")
+
+document.querySelector('#red').addEventListener('click', function(){
+  console.log('Red Button')
+  red.press()
+
   return false
 })
